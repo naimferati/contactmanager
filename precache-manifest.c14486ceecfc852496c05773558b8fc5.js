c216edcb87a58cb51f22a93a95334eb43177a7b0
Naim Ferati <naimferati97@gmail.com>
2020-02-04 14:48:12 +0100

    Updates

---
+++ precache-manifest.c14486ceecfc852496c05773558b8fc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5564b341bf995728970591532a3714f",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "2537e882d09741d62e96",
+    "url": "/contactmanager/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "112f0ea2870da0325c0e",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2537e882d09741d62e96",
+    "url": "/contactmanager/static/js/2.157d0350.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.157d0350.chunk.js.LICENSE"
+  },
+  {
+    "revision": "112f0ea2870da0325c0e",
+    "url": "/contactmanager/static/js/main.71445a84.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
